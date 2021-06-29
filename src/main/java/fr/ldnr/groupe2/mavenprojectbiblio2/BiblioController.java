a3d5595d77983650b9c91f664f35afa03e041cb6
stag <stag@formation-ldnr-5CG62366N4>
2021-06-29 22:05:20 +0200

    Ajout d'un contrôleur pour créer un emprunt

--- src/main/java/fr/ldnr/groupe2/mavenprojectbiblio2/BiblioController.java
+++ src/main/java/fr/ldnr/groupe2/mavenprojectbiblio2/BiblioController.java
@@ -1,5 +1,8 @@
 package fr.ldnr.groupe2.mavenprojectbiblio2;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 import org.hibernate.Session;
@@ -10,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +34,7 @@ public class BiblioController {
     @RequestMapping("/creation")
     @ResponseBody
     public void creation() {
+        //crée des livres et des emprunts de base pour les tests
         Session session = sessionFactory.openSession();
         Livre l1 = new Livre("2015", "A la lumière des étoiles", "Hardy", "Thomas", "CraneFord Editions");
         Livre l2 = new Livre("1932", "Villette", "Brontë", "Charlotte", "CraneFord Editions");
@@ -38,7 +43,7 @@ public class BiblioController {
         Emprunt e1 = new Emprunt(new GregorianCalendar(2021, 6, 29).getTime(), "Justin Bodinier", l4);
         Emprunt e2 = new Emprunt(new GregorianCalendar(2021, 6, 28).getTime(), "Chantal Keda", l3);
         Emprunt e3 = new Emprunt(new GregorianCalendar(2021, 6, 27).getTime(), "Thierry Vuillermet", l1);
-               
+
         Transaction tx = session.beginTransaction(); //ouvre la transaction
         session.save(l1);
         session.save(l2);
@@ -49,24 +54,36 @@ public class BiblioController {
         session.save(e3);
         tx.commit(); //ferme la transaction
     }
-    
+
+    //Contrôleur qui affiche la liste de tous les livres de la bdd 
     @RequestMapping("/bdd/livres")
-	public List<Livre> dirigeable() {
-		Session session = sessionFactory.openSession();
-		String livresHQL = "FROM Livre";
-		List<Livre> list = session.createQuery(livresHQL).list();
-		session.close();
-		return list;
-	}
-        
-        @RequestMapping("/bdd/emprunts")
-	public List<Emprunt> emprunts() {
-		Session session = sessionFactory.openSession();
-		String empruntsHQL = "FROM Emprunt";
-		List<Emprunt> list = session.createQuery(empruntsHQL).list();
-		session.close();
-		return list;
-	}
+    public List<Livre> livres() {
+        Session session = sessionFactory.openSession();
+        String livresHQL = "FROM Livre";
+        List<Livre> list = session.createQuery(livresHQL).list();
+        session.close();
+        return list;
+    }
+
+    //Contrôleur qui affiche la liste de tous les livres de la bdd triés par titre
+    @RequestMapping("/bdd/livrespartitre")
+    public List<Livre> livresParTitre() {
+        Session session = sessionFactory.openSession();
+        String livresParTitreHQL = "FROM Livre ORDER BY titre";
+        List<Livre> list = session.createQuery(livresParTitreHQL).list();
+        session.close();
+        return list;
+    }
+
+    //Contrôleur qui affiche la liste de tous les emprunts dans la base
+    @RequestMapping("/bdd/emprunts")
+    public List<Emprunt> emprunts() {
+        Session session = sessionFactory.openSession();
+        String empruntsHQL = "FROM Emprunt";
+        List<Emprunt> list = session.createQuery(empruntsHQL).list();
+        session.close();
+        return list;
+    }
 
     //Contrôleur qui relie la page html emprunt.html avec l'url /emprunt
     @Controller
@@ -76,6 +93,24 @@ public class BiblioController {
         public String getTestPage() {
             return "emprunt.html";
         }
+    }
+
+    @RequestMapping("/creation/emprunt/{idLivre}/{nomEmprunteur}/{date}")
+    public String creationEmprunt(@PathVariable String idLivre, @PathVariable String nomEmprunteur, @PathVariable String date) throws ParseException {
+        //Récupération de l'objet livre dans la table Livre grâce à son id (qu'il faut caster en int)
+        Session session = sessionFactory.openSession();
+        int idLivreInt = Integer.parseInt(idLivre);
+        Livre livreEmprunt = session.load(Livre.class, idLivreInt);
+        //Cast de la date String récupéré en objet date
+        Date dateEmprunt = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+        //Création d'un nouvel emprunt
+        Emprunt nouvelEmprunt = new Emprunt (dateEmprunt, nomEmprunteur, livreEmprunt);
+        logger.info("Emprunt créé: id du livre n°" + idLivre + ", Emprunteur : " + nomEmprunteur +" à la date du " + date);
+        //Ajout de l'emprunt dans la base
+        session.save(nouvelEmprunt);
+        session.close();
+        logger.info("Nouvel Emprunt ajouté à la base");
+        return idLivre;
     }
 
 }
