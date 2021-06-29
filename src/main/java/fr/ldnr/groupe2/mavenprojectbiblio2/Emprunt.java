d6385d85cf2b356eaecf8c0686a5c0130b781f14
stag <stag@formation-ldnr-5CG62366N4>
2021-06-29 22:04:06 +0200

    Correctif @ManyToOne au dessus du getLivre()

--- src/main/java/fr/ldnr/groupe2/mavenprojectbiblio2/Emprunt.java
+++ src/main/java/fr/ldnr/groupe2/mavenprojectbiblio2/Emprunt.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -39,7 +40,6 @@ public class Emprunt implements Serializable {
 
     
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
     public int getIdEmprunt() {
         return idEmprunt;
     }
@@ -67,7 +67,7 @@ public class Emprunt implements Serializable {
         this.nomEmprunteur = nomEmprunteur;
     }
 
-    @Column(nullable = false)
+    @ManyToOne 
     public Livre getLivre() {
         return livre;
     }
