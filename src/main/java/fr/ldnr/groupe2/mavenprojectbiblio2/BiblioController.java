94ad06e13ccacdf72db1d9af9e8d77688d4d8f7b
stag <stag@formation-ldnr-5CG62366N4>
2021-07-01 08:56:37 +0200

    Implémentation de la page de création de livres + le js associé au contrôleur de création

--- src/main/java/fr/ldnr/groupe2/mavenprojectbiblio2/BiblioController.java
+++ src/main/java/fr/ldnr/groupe2/mavenprojectbiblio2/BiblioController.java
@@ -44,7 +44,6 @@ public class BiblioController {
         Emprunt e2 = new Emprunt(new GregorianCalendar(2021, 6, 28).getTime(), "Chantal Keda", l3);
         Emprunt e3 = new Emprunt(new GregorianCalendar(2021, 6, 27).getTime(), "Thierry Vuillermet", l1);
 
-        Transaction tx = session.beginTransaction(); //ouvre la transaction
         session.save(l1);
         session.save(l2);
         session.save(l3);
@@ -52,7 +51,7 @@ public class BiblioController {
         session.save(e1);
         session.save(e2);
         session.save(e3);
-        tx.commit(); //ferme la transaction
+        session.close();
     }
 
     //Contrôleur qui affiche la liste de tous les livres de la bdd 
@@ -95,7 +94,6 @@ public class BiblioController {
         }
     }
 
-    
     //Contrôleur qui crée un emprunt à partir de l'id d'un livre
     @RequestMapping("/creation/emprunt/{idLivre}/{nomEmprunteur}/{date}")
     public String creationEmprunt(@PathVariable String idLivre, @PathVariable String nomEmprunteur, @PathVariable String date) throws ParseException {
@@ -106,16 +104,15 @@ public class BiblioController {
         //Cast de la date String récupéré en objet date
         Date dateEmprunt = new SimpleDateFormat("yyyy-MM-dd").parse(date);
         //Création d'un nouvel emprunt
-        Emprunt nouvelEmprunt = new Emprunt (dateEmprunt, nomEmprunteur, livreEmprunt);
-        logger.info("Emprunt créé: id du livre n°" + idLivre + ", Emprunteur : " + nomEmprunteur +" à la date du " + date);
+        Emprunt nouvelEmprunt = new Emprunt(dateEmprunt, nomEmprunteur, livreEmprunt);
+        logger.info("Emprunt créé: id du livre n°" + idLivre + ", Emprunteur : " + nomEmprunteur + " à la date du " + date);
         //Ajout de l'emprunt dans la base
         session.save(nouvelEmprunt);
         session.close();
         logger.info("Nouvel Emprunt ajouté à la base");
         return idLivre;
     }
-    
-    
+
     //Contrôleur qui met à jour le statut d'un emprunt à partir de son id
     @RequestMapping("/miseajour/emprunt/{idEmprunt}")
     public String miseAjour(@PathVariable String idEmprunt) throws ParseException {
@@ -132,30 +129,39 @@ public class BiblioController {
         } //cast de l'id de String à Int
         logger.info("Emprunt n° :" + idEmprunt + " mis à jour avec succès!");
         logger.info("Valeur du booléen estEmprunte :" + emprunt.isEstEmprunte());
-        return "Statut de l'emprunt n° "+idEmprunt+" mis à jour";
+        return "Statut de l'emprunt n° " + idEmprunt + " mis à jour";
     }
-    
-     //Contrôleur qui affiche la liste de tous les emprunts dans la base
+
+    //Contrôleur qui affiche la liste de tous les emprunts dans la base
     @RequestMapping("/coucou/{titre}")
     public String coucou(@PathVariable String titre) {
-        logger.info("coucou OK :"+titre);
+        logger.info("coucou OK :" + titre);
         return "coucou";
     }
 
-    
     @RequestMapping("/creation/livre/{titre}/{nomAuteur}/{prenomAuteur}/{annee}/{editeur}")
     public String creationLivre(@PathVariable String titre, @PathVariable String nomAuteur,
             @PathVariable String prenomAuteur, @PathVariable String annee, @PathVariable String editeur) throws ParseException {
         Session session = sessionFactory.openSession();
         //Création d'un nouvel livre
-        Livre nouveauLivre = new Livre (titre, nomAuteur, prenomAuteur, annee, editeur);
-        logger.info("Livre créé: " + ", Titre : " + titre +", Auteur : " + nomAuteur + " " + prenomAuteur + " date de " + annee + " edité par " + editeur);
+        Livre nouveauLivre = new Livre(annee, titre, nomAuteur, prenomAuteur, editeur);
+        logger.info("Livre créé: " + ", Titre : " + titre + ", Auteur : " + nomAuteur + " " + prenomAuteur + " date de " + annee + " edité par " + editeur);
         //Ajout du livre dans la base
         session.save(nouveauLivre);
         session.close();
         logger.info("Nouveau livre ajouté à la base");
         return "ok";
     }
-    
-    
+
+    //Contrôleur qui permet d'afficher la liste des emprunts contenus dans la base triés par date 
+    //Emprunts en cours
+    @RequestMapping("/bdd/listEmprunts")
+    public List<Emprunt> listEmprunts() {
+        Session session = sessionFactory.openSession();
+        String ListeDesEmprunts = "From Emprunt ORDER BY date";
+        List<Emprunt> listEmprunt = session.createQuery(ListeDesEmprunts).getResultList();
+        session.close();
+        return listEmprunt;
+    }
+
 }
