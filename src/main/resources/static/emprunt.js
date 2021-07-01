82e82c88975d5ecafa8eb7b689720f325c9bf266
stag <stag@formation-ldnr-5CG62366N4>
2021-07-01 19:52:13 +0200

    Gestion du rafraîchissement des pages à chaque action

--- src/main/resources/static/emprunt.js
+++ src/main/resources/static/emprunt.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function ($) {
     }).done(function (livresParTitre) { // 200
         //boucle pour afficher les livres dans l'input select du formulaire
         //la valeur de chaque option est l'id du livre
-        if (livresParTitre.length==0) {
+        if (livresParTitre.length == 0) {
             $('#titreListeLivres').html("Pas de livre disponible")
         }
         for (const livre of livresParTitre) {
@@ -28,11 +28,11 @@ jQuery(document).ready(function ($) {
             type: "GET",
             dataType: "json"
         }).done(function (retour) { // 200
-            if (retour.length==0) {
+            if (retour.length === 0) {
                 $("#message").html("Pas d'emprunts en cours pour le moment...");
             } else {
-            $("#message").html("Nombre d'emprunts en cours " + retour.length + " emprunts");
-        }
+                $("#message").html("Nombre d'emprunts en cours " + retour.length + " emprunts");
+            }
             let lignes = "";
             for (const ligne of retour) {
                 lignes += "<tr>" +
@@ -78,14 +78,13 @@ $(function () {
     });
 });
 
-//fonction Jquery qui se lance au clic du bouton envoyer
+//********************AU CLIC DU BOUTON ENVOYER******************************************************************************************************************
 $(function () {
     $("#envoyer").on('click', function () {
         //récupération de l'id du livre sélectionné
         let id = $("#listeLivres option:selected").val();
         let nomEmprunteur = $("#nomEmprunteur").val();
         let date = $("#date").val();
-        //envoi de l'id pour la création d'un emprunt
         $.ajax({
             url: "/creation/emprunt/" + id + "/" + nomEmprunteur + "/" + date,
             type: "GET",
@@ -95,8 +94,60 @@ $(function () {
         }).fail(function () { // 400, 501..
             $("#message2").html("Echec !");
         });
+        $('#formulaire').submit(function (e) {
+            //si les champs du formulaire sont bien remplis
+            e.preventDefault();
+            alert("Emprunt créé avec succès");
+            //envoi de l'id pour la création d'un emprunt
+            $.ajax({
+                url: "bdd/listEmprunts",
+                type: "GET",
+                dataType: "json"
+            }).done(function (retour) { // 200
+                $("#message").html("Nombre d'emprunts dans la base :" + retour.length);
+                let lignes = "";
+                for (const ligne of retour) {
+                    lignes += "<tr>" +
+                            "<td>" + ligne.idEmprunt + "</td>" +
+                            "<td>" + ligne.livre.titre + "</td>" +
+                            "<td>" + ligne.livre.nomAuteur + "</td>" +
+                            "<td>" + ligne.nomEmprunteur + "</td>" +
+                            "<td>" + ligne.date + "</td>" +
+                            "<td><button id='" + ligne.idEmprunt + "' onclick='EmpruntFini(this.id)'>effectuer un retour</button></td></tr>";
+                }
+                $("#listemprunts tbody").html(lignes);
+            }).then(function () {
+                $.ajax({
+                    //récupération de la liste de livres existante dans la bdd triée par titre
+                    url: "/bdd/livrespartitre",
+                    type: "GET",
+                    dataType: "json"
+                }).done(function (livresParTitre) { // 200
+                    //vide la liste  en rajoutant seulement la première option
+                    $('#listeLivres').empty();
+                    $('#listeLivres').append('<option id="titreListeLivres" value="">--Veuillez sélectionner un livre--</option>');
+                    if (livresParTitre.length === 0) {
+                        $('#titreListeLivres').html("Pas de livre disponible");
+                    }
+                    for (const livre of livresParTitre) {
+                        $('#listeLivres').append('<option value="' + livre.idLivre + '">' + livre.titre + ' (' + livre.prenomAuteur + ' ' + livre.nomAuteur + ')</option>');
+                    }
+                }).fail(function () { // 400, 501..
+                    $("#message").html("Echec !");
+                });
+            });
+        });
     });
+
 });
+
+
+//********************************************fin CLIC bouton ENVOYER*******************************************************************
+
+
+
+
+
 
 //fonction au clic d'un bouton action
 function EmpruntFini(id) {
@@ -105,6 +156,50 @@ function EmpruntFini(id) {
         type: "PUT",
         dataType: "json"
     });
+    $(function (e) {
+        //ouverture d'une boite de dialogue avec confirmation
+        if (confirm("Confirmez vous la fin de l'emprunt à la date du jour?")) {
+            $.ajax({
+                url: "bdd/listEmprunts",
+                type: "GET",
+                dataType: "json"
+            }).done(function (retour) { // 200
+                $("#message").html("Nombre d'emprunts dans la base :" + retour.length);
+                let lignes = "";
+                for (const ligne of retour) {
+                    lignes += "<tr>" +
+                            "<td>" + ligne.idEmprunt + "</td>" +
+                            "<td>" + ligne.livre.titre + "</td>" +
+                            "<td>" + ligne.livre.nomAuteur + "</td>" +
+                            "<td>" + ligne.nomEmprunteur + "</td>" +
+                            "<td>" + ligne.date + "</td>" +
+                            "<td><button id='" + ligne.idEmprunt + "' onclick='EmpruntFini(this.id)'>effectuer le retour</button></td></tr>";
+                }
+                $("#listemprunts tbody").html(lignes);
+            }).then(function () {
+                $.ajax({
+                    //récupération de la liste de livres existante dans la bdd triée par titre
+                    url: "/bdd/livrespartitre",
+                    type: "GET",
+                    dataType: "json"
+                }).done(function (livresParTitre) { // 200
+                    //vide la liste  en rajoutant seulement la première option
+                    $('#listeLivres').empty();
+                    $('#listeLivres').append('<option id="titreListeLivres" value="">--Veuillez sélectionner un livre--</option>');
+                    if (livresParTitre.length === 0) {
+                        $('#titreListeLivres').html("Pas de livre disponible");
+                    }
+                    for (const livre of livresParTitre) {
+                        $('#listeLivres').append('<option value="' + livre.idLivre + '">' + livre.titre + ' (' + livre.prenomAuteur + ' ' + livre.nomAuteur + ')</option>');
+                    }
+                }).fail(function () { // 400, 501..
+                    $("#message").html("Echec !");
+                });
+            });
+        } else {
+        }
+    });
+
 }
 
 
